5379e930416a4cda59712b046dd15e744870ec04
Jose Matias Ortuzar <jm.ortuzar@gmail.com>
2020-09-19 01:04:08 -0300

    reorganizar y JWT inicios

--- server/config/config.js
+++ server/config/config.js
@@ -9,3 +9,12 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 //DB
 
 process.env.URLDB = process.env.NODE_ENV === 'dev' ? 'mongodb://localhost:27017/cafe' : process.env.MONGO_URL;
+
+// vancimiento token
+
+process.env.CADUCIDAD_TOKEN = 60 * 60 * 24 * 30;
+
+//SEED
+
+process.env.SEED = process.env.SEED || 'este-es-el-seed-desarrollo';
+
